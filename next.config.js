f9025753d13f4060b8f47ed75b7068ff203e3720
Mahmoud Ashraf <mahmoudashrafsaad8@gmail.com>
2021-04-13 20:00:50 +0200

    PWA V1.3

--- next.config.js
+++ next.config.js
@@ -9,5 +9,6 @@ module.exports = withPWA({
   pwa: {
     dest: "public",
     runtimeCaching,
+    mode: "production",
   },
 });
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+      />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
